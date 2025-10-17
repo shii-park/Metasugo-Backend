2a0ef97d4f30c131d9848addd8bf48a271f193c8
meg44k <yugomegane@gmail.com>
2025-10-17 23:09:55 +0900

    Add: マス効果の方を定義しました

--- internal/service/effect.go
+++ internal/service/effect.go
@@ -1,4 +1,29 @@
 package sugoroku
 
-type Effect struct {
+type Effect interface {
+	Apply(player *Player)
+}
+
+type ProfitEffect struct {
+	Amount int `json: "amount"`
+}
+
+type LossEffect struct {
+	Amount int `json: "amount"`
+}
+
+type QuizEffect struct {
+	QuizID int `json:"quiz_id"`
+}
+
+type OverallEffect struct {
+	Amount int `json: "amount"`
+}
+
+type NeighborEffect struct {
+	Amount int `json: "amount"`
+}
+
+type RequireEffect struct {
+	RequireValue int `json: "require_value"`
 }
