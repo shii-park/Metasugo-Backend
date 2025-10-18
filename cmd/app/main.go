1b49bcd2d8dd9fe574be4ccfa808a3929f97f0bf
Suiren91 <rento8191@gmail.com>
2025-10-18 12:12:42 +0900

    Add: ginのパッケージを追加

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
 
+	"github.com/Metasugo-Backend/internal/handler"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/signup", handler.SignUp)
 }
