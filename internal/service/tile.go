74c73fc378b1d4116f080c6d39de6a778f1540f8
meg44k <yugomegane@gmail.com>
2025-10-18 10:23:00 +0900

    Add: Effectによるマスインスタンス生成の分岐を実装しました

--- internal/service/tile.go
+++ internal/service/tile.go
@@ -11,8 +11,8 @@ const (
 	loss     TileKind = "loss"
 	quiz     TileKind = "quiz"
 	branch   TileKind = "branch"
-	overall  TileKind = "overallEffect"
-	neighbor TileKind = "neighborEffect"
+	overall  TileKind = "overall"
+	neighbor TileKind = "neighbor"
 	require  TileKind = "require"
 	gamble   TileKind = "gamnble"
 )
@@ -26,18 +26,22 @@ type Tile struct {
 	next   *Tile
 	kind   TileKind
 	id     int
-	effect *Effect
+	effect Effect
 	detail string
 }
 
 // JSONの構造に対応した一時的な構造体
 type TileJSON struct {
-	ID     int      `json:"id"`
-	Kind   TileKind `json:"kind"`
-	Detail string   `json:"detail"`
-	Effect Effect   `json:"effect"`
-	PrevID int      `json:"prev_id"`
-	NextID int      `json:"next_id"`
+	ID     int             `json:"id"`
+	Kind   TileKind        `json:"kind"`
+	Detail string          `json:"detail"`
+	Effect json.RawMessage `json:"effect"`
+	PrevID int             `json:"prev_id"`
+	NextID int             `json:"next_id"`
+}
+
+type effectWithType struct {
+	Type TileKind `json:"type"`
 }
 
 //                                            __                                      __
@@ -52,7 +56,7 @@ type TileJSON struct {
 //
 
 // TODO: 完全コンストラクタ化を行うべき
-func NewTile(prev *Tile, next *Tile, kind TileKind, id int, effect *Effect, detail string) *Tile {
+func NewTile(prev *Tile, next *Tile, kind TileKind, id int, effect Effect, detail string) *Tile {
 	return &Tile{
 		prev:   prev,
 		next:   next,
@@ -93,8 +97,63 @@ func InitTiles() []*Tile {
 
 	// タイルを生成(この時点ではタイル同士はつながっていない)
 	for _, tj := range tilesJSON {
-		// TODO エフェクトの記述をしないといけない
-		tile := NewTile(nil, nil, tj.Kind, tj.ID, nil, tj.Detail)
+		var effect Effect
+		// Effectによるマスインスタンス生成の分岐
+		if len(tj.Effect) > 0 {
+			var ewt effectWithType
+			if err := json.Unmarshal(tj.Effect, &ewt); err != nil {
+				log.Fatalf("Effect type unmarshal error: %v", err)
+			}
+			switch ewt.Type {
+			case profit:
+				var profitEffect ProfitEffect
+				if err := json.Unmarshal(tj.Effect, &profitEffect); err != nil {
+					log.Fatalf("ProfitEffect unmarshal error: %v", err)
+				}
+				effect = profitEffect
+			case loss:
+				var lossEffect LossEffect
+				if err := json.Unmarshal(tj.Effect, &lossEffect); err != nil {
+					log.Fatalf("ProfitEffect unmarshal error: %v", err)
+				}
+				effect = lossEffect
+			case quiz:
+				var quizEffect QuizEffect
+				if err := json.Unmarshal(tj.Effect, &quizEffect); err != nil {
+					log.Fatalf("QuizEffect unmarshal error: %v", err)
+				}
+				effect = quizEffect
+			case branch:
+				var branchEffect BranchEffect
+				if err := json.Unmarshal(tj.Effect, &branchEffect); err != nil {
+					log.Fatalf("BranchEffect unmarshal error: %v", err)
+				}
+				effect = branchEffect
+			case overall:
+				var overallEffect OverallEffect
+				if err := json.Unmarshal(tj.Effect, &overallEffect); err != nil {
+					log.Fatalf("OverallEffect unmarshal error: %v", err)
+				}
+				effect = overallEffect
+			case neighbor:
+				var neighborEffect NeighborEffect
+				if err := json.Unmarshal(tj.Effect, &neighborEffect); err != nil {
+					log.Fatalf("NeighborEffect unmarshal error: %v", err)
+				}
+				effect = neighborEffect
+			case require:
+				var requireEffect RequireEffect
+				if err := json.Unmarshal(tj.Effect, &requireEffect); err != nil {
+					log.Fatalf("RequireEffect unmarshal error: %v", err)
+				}
+				effect = requireEffect
+			case gamble:
+				effect = GambleEffect{}
+			default:
+				effect = nil
+			}
+		}
+		tile := NewTile(nil, nil, tj.Kind, tj.ID, effect, tj.Detail)
 		tileMap[tile.id] = tile
 		tiles = append(tiles, tile)
 	}
