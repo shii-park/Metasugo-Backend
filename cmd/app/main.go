bd5b3bd4c73a001516ff8bece9eec495581d0c8f
meg44k <yugomegane@gmail.com>
2025-10-19 21:28:34 +0900

    Add: main関数実行時にgameインスタンスが生成されるようにしました

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import (
+	"github.com/shii-park/Metasugo-Backend/internal/sugoroku"
+)
 
+func main() {
+	game := sugoroku.NewGame() // ハンドラができた際に、gameにAddplayerができるようになる
 }
