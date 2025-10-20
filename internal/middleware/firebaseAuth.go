9b0fc8f0f6ea10d37ca95e5d8d40a3c872c77c7c
Suiren91 <rento8191@gmail.com>
2025-10-20 18:15:57 +0900

    Add: ユーザ認証用ミドルウェアを作成

--- internal/middleware/firebaseAuth.go
+++ internal/middleware/firebaseAuth.go
@@ -1,1 +1,58 @@
 package middleware
+
+import (
+	"context"
+	//"net/http"
+	"strings"
+
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
+	"github.com/gin-gonic/gin"
+	//"google.golang.org/api/option"
+)
+
+var firebaseAuth *auth.Client
+
+func InitFirebase() error {
+
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		return err
+	}
+
+	firebaseAuth, err = app.Auth(context.Background())
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func AuthToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(401, gin.H{"error": "Authorization header required"})
+			c.Abort()
+			return
+		}
+		idToken := strings.TrimPrefix(authHeader, "Bearer ")
+		if idToken == authHeader {
+			c.JSON(401, gin.H{"error": "Invalid authorization format"})
+			c.Abort()
+			return
+		}
+
+		token, err := firebaseAuth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			c.JSON(401, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("firebase_uid", token.UID)
+		c.Set("user_email", token.Claims["email"]) //これは必要ないかも
+		c.Next()
+	}
+}
