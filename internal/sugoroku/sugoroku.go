0dd9d01b3bdca8f53873e4b6c67c623711a4b002
meg44k <yugomegane@gmail.com>
2025-10-20 10:23:58 +0900

    Add: プレイヤーの前後、現在地にいるプレイヤーを取得するメソッドを追加しました

--- internal/sugoroku/sugoroku.go
+++ internal/sugoroku/sugoroku.go
@@ -59,7 +59,7 @@ func (g *Game) AddPlayer(id string) (*Player, error) {
 	return player, nil
 }
 
-func (g *Game) GetPlayers() []*Player {
+func (g *Game) GetAllPlayers() []*Player {
 	g.mu.RLock()
 	defer g.mu.RUnlock()
 
@@ -69,4 +69,35 @@ func (g *Game) GetPlayers() []*Player {
 		playerList = append(playerList, player)
 	}
 	return playerList
+}
+
+func (g *Game) GetNeighbors(p *Player) []*Player { // 計算量がプレイヤー数になってしまうのでリファクタリングできる(Tileにプレイヤー情報をもたせるなど)
+	targetTiles := []*Tile{}
+	targetPlayers := []*Player{}
+	if p.position.prev != nil {
+		targetTiles = append(targetTiles, p.position.prev)
+	}
+	if p.position.next != nil {
+		targetTiles = append(targetTiles, p.position.next)
+	}
+	if p.position != nil {
+		targetTiles = append(targetTiles, p.position)
+	}
+
+	if len(targetTiles) == 0 {
+		return nil
+	}
+
+	for _, Player := range g.GetAllPlayers() {
+		if p.id == Player.id {
+			continue
+		}
+		for _, tile := range targetTiles {
+			if Player.position == tile {
+				targetPlayers = append(targetPlayers, Player)
+				break
+			}
+		}
+	}
+	return targetPlayers
 }
