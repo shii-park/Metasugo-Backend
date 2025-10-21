a7d71370d5110b36596d0cc920750b99e5e48dd2
meg44k <yugomegane@gmail.com>
2025-10-21 09:30:15 +0900

    Add: Hubの起動を実装しました

--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -4,7 +4,7 @@ type Hub struct {
 	clients    map[*Client]bool
 	broadcast  chan []byte
 	register   chan *Client
-	unregister chan []*Client
+	unregister chan *Client
 }
 
 func NewHub() *Hub {
@@ -12,6 +12,29 @@ func NewHub() *Hub {
 		clients:    make(map[*Client]bool),
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
-		unregister: make(chan []*Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					delete(h.clients, client)
+					close(client.send)
+				}
+			}
+		}
 	}
 }
