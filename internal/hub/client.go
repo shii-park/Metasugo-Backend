e520d75ad4a500fbc2968f5097612fd257ba4fd1
meg44k <yugomegane@gmail.com>
2025-10-21 09:35:05 +0900

    Add: Client構造体のコンストラクタを追加しました

--- internal/hub/client.go
+++ internal/hub/client.go
@@ -7,3 +7,11 @@ type Client struct {
 	conn *websocket.Conn
 	send chan []byte
 }
+
+func NewClient(hub *Hub, conn *websocket.Conn) *Client {
+	return &Client{
+		hub:  hub,
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+}
