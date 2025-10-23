ba10b68bf748cf82ba34df0eaad2690bbc1431b3
Suiren91 <rento8191@gmail.com>
2025-10-23 15:16:13 +0900

    Add: ランキング機能のエンドポイントを作成

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -22,7 +22,14 @@ func main() {
 	wsHandler := handler.NewWebSocketHandler(h)
 
 	//いろんなエンドポイントをつくろう
-	router.GET("/ranking", handler.HandleRanking)
+	ranking := router.Group("/ranking")
+	{
+		ranking.POST("/score")        //スコア追加
+		ranking.GET("/top")           //トップランキング取得
+		ranking.GET("/all")           //全体ランキング取得
+		ranking.GET("/user/:user_id") //特定ユーザのスコア取得
+		ranking.GET("/me")            //自分のランクを取得
+	}
 	router.GET("/ws/connection", middleware.AuthToken(), wsHandler.HandleWebSocket)
 
 	sugoroku.NewGame() // ハンドラができた際に、gameにAddplayerができるようになる
