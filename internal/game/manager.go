7cf37bed1370942364dffb3dd1fad83af7bc090c
meg44k <yugomegane@gmail.com>
2025-10-23 22:19:34 +0900

    Add: gameManagerに排他制御を追加しました

--- internal/game/manager.go
+++ internal/game/manager.go
@@ -1,6 +1,8 @@
 package game
 
 import (
+	"sync"
+
 	"github.com/shii-park/Metasugo-Backend/internal/hub"
 	"github.com/shii-park/Metasugo-Backend/internal/sugoroku"
 )
@@ -9,6 +11,7 @@ type GameManager struct {
 	game          *sugoroku.Game
 	hub           *hub.Hub
 	playerClients map[string]*hub.Client
+	mu            sync.RWMutex
 }
 
 func NewGameManager(g *sugoroku.Game, h *hub.Hub) *GameManager {
@@ -19,7 +22,12 @@ func NewGameManager(g *sugoroku.Game, h *hub.Hub) *GameManager {
 	}
 }
 
-func (gm *GameManager) RegisterPlayerClient(userID string, c *hub.Client) {
-	gm.game.AddPlayer(userID)
+func (gm *GameManager) RegisterPlayerClient(userID string, c *hub.Client) error {
+	gm.mu.Lock()
+	defer gm.mu.Unlock()
+	if _, err := gm.game.AddPlayer(userID); err != nil {
+		return err
+	}
 	gm.playerClients[userID] = c
+	return nil
 }
